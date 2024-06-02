e28757f77803f6b2d76966b16e2e4309da2e3176
florent fauchille <florentfauchille@florents-MacBook-Pro.local>
2024-06-02 20:55:19 +0200

    step 1: responsive frontend to react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,52 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSeedling } from "@fortawesome/free-solid-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
-
-function SeedlingIcon() {
-  return <FontAwesomeIcon className="seedling" icon={faSeedling} />;
-}
 
 function App() {
   return (
     <div className="app">
-      <h1>
-        <SeedlingIcon /> Hello SIGL <SeedlingIcon />
-      </h1>
+      <ul className="menu">
+        <li className="brand">
+          <h3>Sotracteur</h3>
+        </li>
+        <li>Catalogue</li>
+        <li>
+          <FontAwesomeIcon icon={fas.faShoppingCart} />
+        </li>
+      </ul>
+      <main>
+        <form className="search-input">
+          <input type="search" autoFocus placeholder="Choisir une commune..." />
+          <button type="submit">
+            <FontAwesomeIcon icon={fas.faSearch} />
+          </button>
+        </form>
+        <div className="search-results">
+          <div className="catalog-item">
+            <img src="https://touslestracteurs.com/images/Case_IH/CX90.jpg" />
+            <div className="content">
+              <span className="category">An example of card item</span>
+              <p>Some description</p>
+              <p>CITY</p>
+            </div>
+            <div className="actions">
+              <button>Rent</button>
+              <a>More...</a>
+            </div>
+          </div>
+          <div class="catalog-item-block"></div>
+          <div class="catalog-item-block"></div>
+          <div class="catalog-item-block"></div>
+          <div class="catalog-item-block"></div>
+          <div class="catalog-item-block"></div>
+          <div class="catalog-item-block"></div>
+          <div class="catalog-item-block"></div>
+          <div class="catalog-item-block"></div>
+          <div class="catalog-item-block"></div>
+          <div class="catalog-item-block"></div>
+        </div>
+      </main>
     </div>
   );
 }
